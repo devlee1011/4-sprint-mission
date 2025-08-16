b85830733eb35923386d74cda6a61651ce681270
LeeJihyeon <devlee1011@gmail.com>
2025-08-16 18:26:08 +0900

    feat(테스트): 유저 서비스 계층 단위 테스트 추가
    CRUD에 대해 성공, 실패 케이스 모두 추가

--- src/test/java/com/sprint/mission/discodeit/service/basic/BasicUserServiceTest.java
+++ src/test/java/com/sprint/mission/discodeit/service/basic/BasicUserServiceTest.java
@@ -7,6 +7,7 @@ import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.exception.user.EmailDuplicateException;
+import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
 import com.sprint.mission.discodeit.exception.user.UsernameDuplicateException;
 import com.sprint.mission.discodeit.mapper.UserMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
@@ -19,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -56,16 +58,24 @@ class BasicUserServiceTest {
                         "txt",
                         new byte[]{}
                 ));
-
+        
+        // 이메일 중복 검증 통과
         given(userRepository.existsByEmail(anyString()))
                 .willReturn(false);
+        
+        // 이름 중복 검증 통과
         given(userRepository.existsByUsername(anyString()))
                 .willReturn(false);
-
-        given(binaryContentSaveUtility.toNullableFile(any(Optional.class)))
+        
+        // 프로필 파일 생성 
+        given(binaryContentSaveUtility.toNullableFile(any()))
                 .willReturn(new BinaryContent());
+        
+        // 유저 저장
         given(userRepository.save(any(User.class)))
                 .willReturn(new User());
+        
+        // Dto 매핑
         UserDto response = new UserDto(
                 UUID.randomUUID(),
                 "lucky",
@@ -75,6 +85,7 @@ class BasicUserServiceTest {
         );
         given(userMapper.toDto(any(User.class)))
                 .willReturn(response);
+
         //when
         UserDto actualResponse = userService.create(userCreateRequest, optionalBinaryContentCreateRequest);
 
@@ -102,6 +113,7 @@ class BasicUserServiceTest {
                         new byte[]{}
                 ));
 
+        // 이메일 중복 검증 실패 -> EmailDuplicateException
         given(userRepository.existsByEmail(anyString()))
                 .willReturn(true);
 
@@ -125,9 +137,12 @@ class BasicUserServiceTest {
                         "txt",
                         new byte[]{}
                 ));
-
+        
+        // 이메일 중복 검증 통과
         given(userRepository.existsByEmail(anyString()))
                 .willReturn(false);
+
+        // 이름 중복 검증 실패 -> UsernameDuplicateException
         given(userRepository.existsByUsername(anyString()))
                 .willReturn(true);
 
@@ -137,13 +152,16 @@ class BasicUserServiceTest {
     }
 
     @Test
-    @DisplayName("유저를 찾을 수 있습니다.")
+    @DisplayName("유저를 상세 조회할 수 있습니다.")
     void find_user_success_unit_test() {
         // given
         UUID userId = UUID.randomUUID();
-
+    
+        // 존재하는 유저
         given(userRepository.findById(userId))
                 .willReturn(Optional.of(new User("kkumi", "kkumi@cat.cat", "kkumi1234", null)));
+
+        // Dto 변환
         UserDto response = new UserDto(
                 userId,
                 "kkumi",
@@ -167,11 +185,52 @@ class BasicUserServiceTest {
     }
 
     @Test
-    void findAll() {
+    @DisplayName("존재하지 않는 유저는 상세 조회할 수 없습니다.")
+    void find_user_fail_exception_user() {
+        // given
+        UUID userId = UUID.randomUUID();
+
+        // 존재하지 않는 유저 -> UserNotFoundException
+        given(userRepository.findById(userId))
+                .willReturn(Optional.empty());
+
+        // when, then
+        assertThrows(UserNotFoundException.class,
+                () -> userService.find(userId));
     }
 
     @Test
-    @DisplayName("유저를 수정할 수 있습니다.")
+    @DisplayName("유저 목록을 조회할 수 있습니다.")
+    void findAll_user_success_unit_test() {
+        // given
+        given(userRepository.findAllWithProfileAndStatus())
+                .willReturn(List.of(new User()));
+        
+        // Dto 변환 수행
+        UserDto response = new UserDto(
+                UUID.randomUUID(),
+                "kkumi",
+                "kkumi@cat.cat",
+                null,
+                true
+        );
+        given(userMapper.toDto(any(User.class)))
+                .willReturn(response);
+
+        // when
+        List<UserDto> actualResponse = userService.findAll();
+
+        // then
+        assertAll(
+                () -> assertEquals(response.id(), actualResponse.get(0).id()),
+                () -> assertEquals(response.username(), actualResponse.get(0).username()),
+                () -> assertEquals(response.email(), actualResponse.get(0).email()),
+                () -> assertEquals(response.online(), actualResponse.get(0).online())
+        );
+    }
+
+    @Test
+    @DisplayName("유저 정보를 수정할 수 있습니다.")
     void update_user_success_unit_test() {
         // given
         UUID userId = UUID.randomUUID();
@@ -188,24 +247,26 @@ class BasicUserServiceTest {
         String newUsername = userUpdateRequest.newUsername();
         String newEmail = userUpdateRequest.newEmail();
         String newPassword = userUpdateRequest.newPassword();
-        
-        // 수정할 유저 리포지터리에서 id를 기반으로 가져오기
+
+        // 존재하는 유저
         given(userRepository.findById(userId)).willReturn(Optional.of(user));
         
-        // 이메일, 이름 중복 검사
+        // 이메일 중복 검증 통과
         given(userRepository.existsByEmail(anyString()))
                 .willReturn(false);
+        
+        // 이름 중복 검증 통과
         given(userRepository.existsByUsername(anyString()))
                 .willReturn(false);
 
         // 프로필 파일 저장
-        given(binaryContentSaveUtility.toNullableFile(any(Optional.class)))
+        given(binaryContentSaveUtility.toNullableFile(any()))
                 .willReturn(new BinaryContent());
 
         // 업데이트
         user.update(newUsername, newEmail, newPassword, null);
 
-        // 매퍼
+        // Dto 변환 수행
         UserDto response = new UserDto(
                 userId,
                 "kkumi2",
@@ -229,6 +290,112 @@ class BasicUserServiceTest {
     }
 
     @Test
-    void delete() {
+    @DisplayName("존재하지 않는 유저의 정보는 수정할 수 없습니다.")
+    void update_user_fail_exception_user() {
+        // given
+        UUID userId = UUID.randomUUID();
+
+        UserUpdateRequest userUpdateRequest = new UserUpdateRequest(
+                "kkumi2",
+                "kkumi2@cat.cat",
+                "kkumi12345"
+        );
+
+        // 존재하지 않는 유저-> UserNotFoundException
+        given(userRepository.findById(userId)).willReturn(Optional.empty());
+
+        // when, then
+        assertThrows(UserNotFoundException.class,
+                () -> userService.update(userId, userUpdateRequest, Optional.empty()));
+        
+    }
+
+    @Test
+    @DisplayName("중복된 이메일로 수정할 수 없습니다.")
+    void update_user_fail_exception_email() {
+        // given
+        UUID userId = UUID.randomUUID();
+        User user = new User();
+
+        UserUpdateRequest userUpdateRequest = new UserUpdateRequest(
+                "kkumi2",
+                "kkumi2@cat.cat",
+                "kkumi12345"
+        );
+
+        // 존재하는 유저
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+
+        // 이메일 중복 검증 실패 -> EmailDuplicateException
+        given(userRepository.existsByEmail(anyString()))
+                .willReturn(true);
+
+        // when, then
+        assertThrows(EmailDuplicateException.class,
+                () -> userService.update(userId, userUpdateRequest, Optional.empty()));
+
+    }
+
+    @Test
+    @DisplayName("중복된 이름으로 수정할 수 없습니다.")
+    void update_user_fail_exception_username() {
+        // given
+        UUID userId = UUID.randomUUID();
+        User user = new User();
+
+        UserUpdateRequest userUpdateRequest = new UserUpdateRequest(
+                "kkumi2",
+                "kkumi2@cat.cat",
+                "kkumi12345"
+        );
+
+        // 존재하는 유저
+        given(userRepository.findById(userId)).willReturn(Optional.of(user));
+
+        // 이메일 중복 검증 통과
+        given(userRepository.existsByEmail(anyString()))
+                .willReturn(false);
+
+        // 이름 중복 검증 실패 -> UsernameDuplicateException
+        given(userRepository.existsByUsername(anyString()))
+                .willReturn(true);
+
+        // when, then
+        assertThrows(UsernameDuplicateException.class,
+                () -> userService.update(userId, userUpdateRequest, Optional.empty()));
+
+    }
+
+    @Test
+    @DisplayName("유저를 삭제할 수 있습니다.")
+    void delete_user_success_unit_test() {
+        // given
+        UUID userId = UUID.randomUUID();
+        
+        // 존재하는 유저
+        given(userRepository.existsById(userId))
+                .willReturn(true);
+
+        // when
+        userService.delete(userId);
+
+        // then
+        // 삭제 수행(1회만)
+        verify(userRepository, times(1)).deleteById(userId);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 유저는 삭제할 수 없습니다.")
+    void delete_user_fail_exception_user() {
+        // given
+        UUID userId = UUID.randomUUID();
+
+        // 존재하지 않는 유저 -> UserNotFoundException
+        given(userRepository.existsById(userId))
+                .willReturn(false);
+
+        // when, then
+        assertThrows(UserNotFoundException.class,
+                () -> userService.delete(userId));
     }
 }
