3686368bc68be78bbef9176189791778cb1b38bc
JihyeonLee <devlee1011@gmail.com>
2025-09-29 11:42:04 +0900

    feat(security): 세션 ID를 통해 사용자의 기본 정보를 가져오는 API 추가

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.auth.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.controller.api.AuthApi;
+import com.sprint.mission.discodeit.dto.data.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
@@ -21,5 +24,13 @@ public class AuthController implements AuthApi {
         return ResponseEntity
                 .status(HttpStatus.NON_AUTHORITATIVE_INFORMATION)
                 .body(null);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getCurrentUser(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+        if (userDetails == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok(userDetails.getUserDto());
     }
 }
